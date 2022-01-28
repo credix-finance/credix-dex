628ab34e9db19a69aea94ab21025bca2b78416c8
Ruven Salamon <ruven.salamon@gmail.com>
2022-01-28 23:06:53 +0100

    iqwq

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,12 @@
-import { Commitment } from "@solana/web3.js";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import {
-  WalletModalProvider,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import {
-  Layout,
-  Row,
-  Col,
-  Button,
-  Divider,
-  Checkbox,
-  Card,
-  Tabs,
-  Input,
-  InputNumber,
-  Image,
-} from "antd";
+import { Commitment } from "@solana/web3.js";
 import type { NextPage } from "next";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { Dex } from "../components/Dex";
 
 require("@solana/wallet-adapter-react-ui/styles.css");
@@ -36,47 +20,8 @@ export const config = {
   },
 };
 
-enum OrderType {
-  BUY,
-  SELL,
-}
-
-// JUST FOR DEVELOPPING
-const testOrders = [
-  {
-    size: 3000,
-    price: 34234,
-    type: OrderType.SELL,
-  },
-  {
-    size: 12232,
-    price: 4343434,
-    type: OrderType.BUY,
-  },
-  {
-    size: 3000,
-    price: 34234,
-    type: OrderType.BUY,
-  },
-  {
-    size: 12232,
-    price: 4343434,
-    type: OrderType.SELL,
-  },
-];
-
-/* const Order = ({ size, price }) => {
-  return (
-  );
-}; */
-
 const Home: NextPage = () => {
-  const [limitPrice, setLimitPrice] = useState<number>();
-  const [amount, setAmount] = useState<number>();
-  const [buyTabActive, setBuyTabActive] = useState<boolean>(true);
-  const { Header, Footer, Content } = Layout;
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
-  const [orders, setOrders] = useState(testOrders);
 
   return (
     <ConnectionProvider
